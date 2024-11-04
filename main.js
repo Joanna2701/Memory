fad7522943ed7d98aec1364f785e990b84b94c52
Joanna Leprat <joanna.leprat@fms-ea.com>
2024-11-04 16:28:31 +0100

    on a mis les 6cartes

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-let container = document.querySelector('container');
-container.style.backgroundColor = 'red';
+let container_cards = document.querySelector("#container_cards");
+const arrSrc = [
+  "./img/batte.png",
+  "./img/bonbons.png",
+  "./img/chapeau.png",
+  "./img/citrouille.png",
+  "./img/cyclope.png",
+  "./img/monstre.png",
+];
+
+function createCard(arrSrc) {
+  for (let index = 0; index < arrSrc.length; index++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = arrSrc[index];
+    div.append(img);
+    container_cards.append(div);
+  }
+}
+
+createCard(arrSrc);
