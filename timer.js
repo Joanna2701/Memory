cf03122c22172a2d7828e996c5e2cc848c455718
Longueville <155456691+Longuevville@users.noreply.github.com>
2024-11-06 10:49:47 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -3,71 +3,12 @@ let timerInterval; // Intervales de secondes
 let timerStarted = false; // Évite de relancer plusieurs fois
 
 
-const cardsArray = [  "./img/batte.png",
-    "./img/bonbons.png",
-    "./img/chapeau.png",
-    "./img/citrouille.png",
-    "./img/cyclope.png",
-    "./img/monstre.png",
-    "./img/batte.png",
-    "./img/bonbons.png",
-    "./img/chapeau.png",
-    "./img/citrouille.png",
-    "./img/cyclope.png",
-    "./img/monstre.png",
 
-
-];
-
-//Je sélectionne le container pour afficher les cartes dans le dom 
-
-const gamerBoard = document.querySelector(".container_cards");
-
-//Je créer et affiche des cartes dynamiquement
-
-function createCards() {
-    cardsArray.forEach((src) =>{
-        //Element carte crée
-        const card= document.createElement("div");
-        card.classList.add("card");
-
-
-        // Creation de l'objet interne pour retourner la carte
-        const cardInner = document.createElement("div");
-        cardFront.classList.add("card-front");
-
-        //Je créer la face arrière de la carte à ce moment la
-        const cardBack = document.createElement("div");
-        cardBack.classLlist.add("card-back");
-
-        //J'ajoute l'image de la face arrière de la carte
-        const img = document.createElement("img");
-        img.src = src;
-        img.alt = "image de la carte ouais";
-        cardBack.appendChild(img); //Element enfant
-
-        //J'assemble les parties de la carte
-        cardInner.appendChild(cardFront);
-        cardInner.appendChild(cardBack);
-        card.appendChild(cardInner);
-
-        //Ajoute une carte à la grille du jeu
-        gamerBoard.appendChild(card);
-
-        //Ajoute un évènement de clic pour retourner une carte je ne sais pas si ça fonctionne
-        card.addEventListener("click", () => {
-            card.classList.toggle("flipped");
-            // Il faudra appeler la logique de vérification ici , checkformath ? ETc que sais je... 
-        })
-    })
-}
-
-// Maintenant j'appel la fonction d'affichage des cartes du jeu
-
-createCards();
 
 
 let timer = document.querySelector("#timer");
+
+
 
 // Fonction qui démarre le timer
 function startTimer() {
@@ -83,20 +24,6 @@ function stopTimer() {
     clearInterval(timerInterval);
 }
 
-// Gestion du clic pour démarrer le timer
-//document.addEventListener("DOMContentLoaded", () => {
-    //const timerElement = document.getElementById("timer");
-    //if (timerElement) {
-        //timerElement.addEventListener("click", () => {
-            //console.log("Timer démarré !");
-            //if (!timerStarted) {
-                //startTimer();
-                //timerStarted = true;
-                //timerElement.style.cursor = "default";
-            //}
-        //});
-    //}
-//});
 
 // Vérifie si le jeu est terminé
 function checkGameEnd() {
